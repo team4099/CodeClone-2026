1177b03b00ad466a2fcac53624cfa9482b7f0023
ImJustinLau <jlau@team4099.com>
2026-06-11 20:06:12 -0400

    doing imports and restructuring stuff

--- src/main/kotlin/com/team4099/robot2026/subsystems/superstructure/Intake/LinearIntakeIO.kt
+++ src/main/kotlin/com/team4099/robot2026/subsystems/superstructure/Intake/LinearIntakeIO.kt
@@ -1,18 +1,45 @@
+import org.littletonrobotics.junction.LogTable
+import org.littletonrobotics.junction.inputs.LoggableInputs
+import org.team4099.lib.units.base.Length
+import org.team4099.lib.units.base.Meter
+import org.team4099.lib.units.base.amps
+import org.team4099.lib.units.base.celsius
+import org.team4099.lib.units.base.inAmperes
+import org.team4099.lib.units.base.inCelsius
+import org.team4099.lib.units.base.inInches
+import org.team4099.lib.units.base.inches
+import org.team4099.lib.units.derived.AccelerationFeedforward
+import org.team4099.lib.units.derived.DerivativeGain
+import org.team4099.lib.units.derived.ElectricalPotential
+import org.team4099.lib.units.derived.IntegralGain
+import org.team4099.lib.units.derived.ProportionalGain
+import org.team4099.lib.units.derived.StaticFeedforward
+import org.team4099.lib.units.derived.VelocityFeedforward
+import org.team4099.lib.units.derived.Volt
+import org.team4099.lib.units.derived.inVolts
+import org.team4099.lib.units.derived.volts
+import org.team4099.lib.units.inInchesPerSecond
+import org.team4099.lib.units.perSecond
+
+
 
 interface LinearIntakeIO{
-  class LintakeIOInputs : LoggableInputs{
+
+  class LintakeIOInputs : LoggableInputs {
     var lintakeTemperature = 0.0.celsius
     var lintakeStratorCurrent = 0.amps
     var lintakeSupplyCurrent = 0.amps
+    var lintakePosition = 0.inches
+    var lintakeVelocity = 0.inches.perSecond
     var lintakeAppliedVoltage = 0.volts
-    var lintakeVelocity = 0.0.inches.perSecond
-    var lintakePosition = 0.0.inces
+
+
     override fun toLog(table: LogTable?){
       table?.put("lintakeTemperatureCelsius", lintakeTemperature.inCelsius)
-      table?.put("lintakeStatorCurrentAmps", lintakeStatorCurrent.inAmperes)
+      table?.put("lintakeStratorCurrentAmps", lintakeStratorCurrent.inAmperes)
       table?.put("lintakeSupplyCurrentAmps", lintakeSupplyCurrent.inAmperes)
       table?.put("lintakePositionInches", lintakePosition.inInches)
-      table?.put("lintakeVelocityInchesPerMin", lintakeVelocity.inInchesPerMinute)
+      table?.put("lintakeVelocityInchesPerSec", lintakeVelocity.inInchesPerSecond)
       table?.put("lintakeAppliedVolts", lintakeAppliedVoltage.inVolts)
 
     }
@@ -20,10 +47,10 @@ interface LinearIntakeIO{
       table?.get("lintakeTemperatureCelsius", lintakeTemperature.inCelsius)?.let {
         lintakeTemperature = it.celsius
       }
-      table?.get("lintakeStatorCurrentAmps", lintakeStratorCurrent.iAmperes)?.let {
+      table?.get("lintakeStatorCurrentAmps", lintakeStratorCurrent.inAmperes)?.let {
         lintakeStratorCurrent = it.amps
       }
-      table?.get("lintakeSupplyCurrentAmps", lintakeSupplyCurrent.iAmperes)?.let {
+      table?.get("lintakeSupplyCurrentAmps", lintakeSupplyCurrent.inAmperes)?.let {
         lintakeSupplyCurrent = it.amps
       }
       table?.get("lintakeAppliedVolts", lintakeAppliedVoltage.inVolts)?.let {
@@ -31,23 +58,24 @@ interface LinearIntakeIO{
       }
       table?.get("lintakePositionInches", lintakePosition.inInches)?.let{
         lintakePosition = it.inches}
-      table?.get("lintakeVelocityInchesPerMin", lintakeVelocity.inInchesPerMinute)?.let{
-        lintakeVelocity = it.inches.perMinute}
+      table?.get("lintakeVelocityInchesPerSec", lintakeVelocity.inInchesPerSecond)?.let{
+        lintakeVelocity = it.inches.perSecond}
     }
-    fun updateInputs(inputs: LintakeIOInputs) {}
-    fun setVoltage(voltage: ElectricalPotential) {}
-    fun setPosition(position: Length) {}
-    fun configPID(
-      kP: ProportionalGain<Inches, Volt>,
-      kI: IntegralGain<Inches, Volt>,
-      kD: DerivativeGain<Inches, Volt>
-    ) {}
-    fun configFF(
-      kG: ElectricalPotential,
-      kS: StaticFeedforward<Volt>,
-      kV: VelocityFeedforward<Inches, Volt>,
-      kA: AccelerationFeedforward<Inches, Volt>
-    ) {}
-    fun setBrakeMode(brake: Boolean) {}
+
   }
+  fun updateInputs(inputs: LintakeIOInputs) {}
+  fun setVoltage(voltage: ElectricalPotential) {}
+  fun setPosition(position: Length) {}
+  fun configPID(
+    kP: ProportionalGain<Meter, Volt>,
+    kI: IntegralGain<Meter, Volt>,
+    kD: DerivativeGain<Meter, Volt>
+  ) {}
+  fun configFF(
+    kG: ElectricalPotential,
+    kS: StaticFeedforward<Volt>,
+    kV: VelocityFeedforward<Meter, Volt>,
+    kA: AccelerationFeedforward<Meter, Volt>
+  ) {}
+  fun setBrakeMode(brake: Boolean) {}
 }
--- src/main/kotlin/com/team4099/robot2026/subsystems/superstructure/Intake/Rollers/IntakeRollersIO.kt
+++ src/main/kotlin/com/team4099/robot2026/subsystems/superstructure/Intake/Rollers/IntakeRollersIO.kt
@@ -1,5 +1,22 @@
+import org.littletonrobotics.junction.LogTable
+import org.littletonrobotics.junction.inputs.LoggableInputs
+import org.team4099.lib.units.base.amps
+import org.team4099.lib.units.base.celsius
+import org.team4099.lib.units.base.inAmperes
+import org.team4099.lib.units.base.inCelsius
+import org.team4099.lib.units.derived.Angle
+import org.team4099.lib.units.derived.ElectricalPotential
+import org.team4099.lib.units.derived.inVolts
+import org.team4099.lib.units.derived.rotations
+import org.team4099.lib.units.derived.volts
+import org.team4099.lib.units.inRotationsPerMinute
+import org.team4099.lib.units.inRotationsPerMinutePerMinute
+import org.team4099.lib.units.perMinute
+
+
+
 interface IntakeRollersIO{
-  class RollerInputs : LoggableInputs{
+  class RollerInputs : LoggableInputs {
     var rollerVelocity = 0.rotations.perMinute
     var rollerAcceleration = 0.rotations.perMinute.perMinute
     var rollerAppliedVoltage = 0.volts
@@ -21,16 +38,20 @@ interface IntakeRollersIO{
         rollerStatorCurrent = it.amps}
       table?.get("rollerSupplyCurrent", rollerSupplyCurrent.inAmperes)?.let{
         rollerSupplyCurrent = it.amps}
-      table?.get("rollerAccelerationRotationsPerMinPerMin", rollerAcceleration.inRotationsPerMinutePerMinute){
-        rollerAcceleration = it.rotations.perMiniute.perMinute }
-      table?.get("rollerVelocity", rollerVelocity.inRotationsPerMinute){
+
+      table?.get("rollerAccelerationRotationsPerMinPerMin", rollerAcceleration.inRotationsPerMinutePerMinute)?.let{
+        rollerAcceleration = it.rotations.perMinute.perMinute }
+
+      table?.get("rollerVelocity", rollerVelocity.inRotationsPerMinute)?.let{
         rollerVelocity = it.rotations.perMinute }
-      table?.get("rollerVoltage", rollerAppliedVoltage.inVolts){
+
+      table?.get("rollerVoltage", rollerAppliedVoltage.inVolts)?.let{
         rollerAppliedVoltage = it.volts }
 
     }
-    fun updateInputs(inputs: RollerInputs) {}
-    fun setVoltage(voltage: ElectricalPotential) {}
-    
+
+
   }
+  fun updateInputs(inputs: RollerInputs) {}
+  fun setVoltage(voltage: ElectricalPotential) {}
 }
