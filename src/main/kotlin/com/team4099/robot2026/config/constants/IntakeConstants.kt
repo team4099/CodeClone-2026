f1e7c61cf02609b46a47c6049eeb7bdd509a2fd0
ImJustinLau <jlau@team4099.com>
2026-06-13 12:56:37 -0400

    Status Signals and init for LintakeTalon

--- src/main/kotlin/com/team4099/robot2026/config/constants/IntakeConstants.kt
+++ src/main/kotlin/com/team4099/robot2026/config/constants/IntakeConstants.kt
@@ -1,5 +1,7 @@
 import org.team4099.lib.units.base.inches
 import org.team4099.lib.units.derived.volts
+import org.team4099.lib.units.perSecond
+
 
 object IntakeConstants{
   object LinearIntakeConstants{
@@ -8,6 +10,8 @@ object IntakeConstants{
     val GEAR_RATIO: Double = 1.0 / 1.0
     val VOLTAGE_COMPENSATION = 12.0.volts
     val DIAMETER = 1.0.inches
+    val MAX_ACCELERATION = 0.0.inches.perSecond.perSecond
+    val MAX_VELOCITY = 0.0.inches.perSecond
   }
   object RollerIntakeConstants{
     val IDLE_VOLTAGE = 0.0.volts
--- src/main/kotlin/com/team4099/robot2026/subsystems/superstructure/Intake/LinearIntakeIOTalon.kt
+++ src/main/kotlin/com/team4099/robot2026/subsystems/superstructure/Intake/LinearIntakeIOTalon.kt
@@ -1,10 +1,22 @@
+import com.ctre.phoenix6.StatusSignal
 import com.ctre.phoenix6.configs.TalonFXConfiguration
 import com.ctre.phoenix6.controls.MotionMagicVoltage
 import com.ctre.phoenix6.hardware.TalonFX
+import com.ctre.phoenix6.signals.NeutralModeValue
 import com.team4099.robot2026.config.constants.Constants
+import edu.wpi.first.units.measure.Angle as WPIAngle
+import edu.wpi.first.units.measure.AngularAcceleration as WPIAngularAcceleration
+import edu.wpi.first.units.measure.AngularVelocity as WPIAngularVelocity
+import edu.wpi.first.units.measure.Current as WPICurrent
+import edu.wpi.first.units.measure.Temperature as WPITemperature
+import edu.wpi.first.units.measure.Voltage as WPIVoltage
 import org.team4099.lib.units.ctreLinearMechanismSensor
 import org.team4099.lib.units.derived.degrees
 import org.team4099.lib.units.derived.inDegrees
+import org.team4099.lib.units.inDegreesPerSecond
+import org.team4099.lib.units.inInchesPerSecond
+import org.team4099.lib.units.inInchesPerSecondPerSecond
+
 
 object LinearIntakeIOTalon: LinearIntakeIO {
   private val LintakeTalon: TalonFX = TalonFX(Constants.Lintake.LINTAKE_MOTOR_ID) //Create Motor
@@ -16,4 +28,42 @@ object LinearIntakeIOTalon: LinearIntakeIO {
     IntakeConstants.LinearIntakeConstants.DIAMETER,
     IntakeConstants.LinearIntakeConstants.VOLTAGE_COMPENSATION
   )
+
+  var statorCurrentSignal: StatusSignal<WPICurrent>
+  var supplyCurrentSignal: StatusSignal<WPICurrent>
+  var tempSignal: StatusSignal<WPITemperature>
+  var dutyCycleSignal: StatusSignal<Double>
+  var motorTorqueSignal: StatusSignal<WPICurrent>
+  var motorVoltageSignal: StatusSignal<WPIVoltage>
+  var motorAccelSignal: StatusSignal<WPIAngularAcceleration>
+  var rotorPositionSignal: StatusSignal<WPIAngle>
+  var rotorVelocitySignal: StatusSignal<WPIAngularVelocity>
+  init {
+    LintakeTalon.clearStickyFaults()
+
+
+    configs.CurrentLimits.SupplyCurrentLimit = 40.0
+    configs.CurrentLimits.StatorCurrentLimit = 40.0
+    configs.CurrentLimits.SupplyCurrentLimitEnable = true
+    configs.CurrentLimits.StatorCurrentLimitEnable = true
+    configs.MotorOutput.NeutralMode = NeutralModeValue.Brake
+
+
+    configs.MotionMagic.MotionMagicCruiseVelocity = IntakeConstants.LinearIntakeConstants.MAX_VELOCITY.inInchesPerSecond
+    configs.MotionMagic.MotionMagicAcceleration = IntakeConstants.LinearIntakeConstants.MAX_ACCELERATION.inInchesPerSecondPerSecond
+
+    LintakeTalon.configurator.apply(configs)
+    rotorPositionSignal = LintakeTalon.position
+    rotorVelocitySignal = LintakeTalon.velocity
+    statorCurrentSignal = LintakeTalon.statorCurrent
+    supplyCurrentSignal = LintakeTalon.supplyCurrent
+    tempSignal = LintakeTalon.deviceTemp
+    dutyCycleSignal = LintakeTalon.dutyCycle
+    motorTorqueSignal = LintakeTalon.torqueCurrent
+    motorVoltageSignal = LintakeTalon.motorVoltage
+    motorAccelSignal = LintakeTalon.acceleration
+
+    zeroEncoder()
+  }
+
 }
